d231252e22809dff5d3811e31c8818b5133aab99
Prateek K. Keshari <prateekkeshari7@gmail.com>
2024-06-27 23:54:16 +0200

    ui-color-change

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,14 @@
 import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-  ],
+	],
+  prefix: "",
   theme: {
     container: {
       center: true,
@@ -74,6 +75,6 @@ const config: Config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+} satisfies Config
 
 export default config
